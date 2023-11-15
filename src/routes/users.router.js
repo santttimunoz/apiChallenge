f2d435facbde7c6b78ebf82316bebffcd74934c8
mario robayo <marioferyhwh@gmail.com>
2023-11-14 21:56:10 -0500

    add jwt y roles

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,13 +1,25 @@
 import express from "express";
 import { UserControl } from "../controllers/UserControl.js";
 import { validateUser } from "../middlewares/validateUser.js";
+import { validateJWT } from "../middlewares/validateJWT.js";
+import { validateRoles } from "../middlewares/validateRoles.js";
 
 let controlUser = new UserControl();
 
 export let routesUser = express.Router();
 
-routesUser.post("/api/users", validateUser, controlUser.signupUser);
-routesUser.delete("/api/users/:id", controlUser.deleteUser);
-routesUser.put("/api/users/:id", controlUser.updateUser);
-routesUser.get("/api/users/:id", controlUser.searchUser);
-routesUser.get("/api/users", controlUser.SearcAllUsers);
+routesUser.post(
+  "/api/users",
+  validateJWT,
+  validateUser,
+  controlUser.signupUser
+);
+routesUser.delete("/api/users/:id", validateJWT, controlUser.deleteUser);
+routesUser.put("/api/users/:id", validateJWT, controlUser.updateUser);
+routesUser.get("/api/users/:id", validateJWT, controlUser.searchUser);
+routesUser.get(
+  "/api/users",
+  validateJWT,
+  validateRoles(["admin"]),
+  controlUser.SearcAllUsers
+);
