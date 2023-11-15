93c2e6938115900b3a01fc672c8147c4ba30f8d1
mario robayo <marioferyhwh@gmail.com>
2023-11-14 19:57:01 -0500

    formatea

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,35 +1,34 @@
-import express from "express"
-import { AccountControl } from "../controllers/accountControl.js"
-import { LoginControl } from "../controllers/LoginControl.js"
-import { TeamControl } from "../controllers/TeamControl.js"
+import express from "express";
+import { AccountControl } from "../controllers/accountControl.js";
+import { LoginControl } from "../controllers/LoginControl.js";
+import { TeamControl } from "../controllers/TeamControl.js";
 
 //middlewares
-import { validateAccount } from "../middlewares/validateAccount.js"
-import { validateTeam } from "../middlewares/validateTeam.js"
-import { routesUser } from "./users.router.js"
+import { validateAccount } from "../middlewares/validateAccount.js";
+import { validateTeam } from "../middlewares/validateTeam.js";
+import { routesUser } from "./users.router.js";
 
-let accountControl = new AccountControl()
-let loginControl = new LoginControl()
-let teamControl = new TeamControl()
+let accountControl = new AccountControl();
+let loginControl = new LoginControl();
+let teamControl = new TeamControl();
 
-export let routes = express.Router()
-//aqui van las validaciones 
- routes.use(routesUser);
- routes.post("/api/accounts", validateAccount, accountControl.signupAccount)
- routes.delete("/api/accounts/:id", accountControl.deleteAccount)
- routes.put("/api/accounts/:id", accountControl.updateAccount)
- routes.get("/api/accounts/:id", accountControl.searchAccount)
+export let routes = express.Router();
+//aqui van las validaciones
+routes.use(routesUser);
+routes.post("/api/accounts", validateAccount, accountControl.signupAccount);
+routes.delete("/api/accounts/:id", accountControl.deleteAccount);
+routes.put("/api/accounts/:id", accountControl.updateAccount);
+routes.get("/api/accounts/:id", accountControl.searchAccount);
 
- routes.post("/api/login", loginControl.Login)
- routes.post("/api/logout", loginControl.Logout)
+routes.post("/api/login", loginControl.Login);
+routes.post("/api/logout", loginControl.Logout);
 
- routes.post("/api/team", validateTeam, teamControl.SignupTeam)
- routes.delete("/api/teamdeleted/:id", teamControl.DeleteTeam)
- routes.put("/api/teamupdate/:id", teamControl.UpdateTeam)
- routes.get("/api/showteams", teamControl.SearchTeams)
- routes.get("/api/showmembers/:id", teamControl.ShowMembers)
- routes.get("/api/teamMove", teamControl.ShowTeamMove)
- 
+routes.post("/api/team", validateTeam, teamControl.SignupTeam);
+routes.delete("/api/teamdeleted/:id", teamControl.DeleteTeam);
+routes.put("/api/teamupdate/:id", teamControl.UpdateTeam);
+routes.get("/api/showteams", teamControl.SearchTeams);
+routes.get("/api/showmembers/:id", teamControl.ShowMembers);
+routes.get("/api/teamMove", teamControl.ShowTeamMove);
 
 //  {
 //     "nombre" : "jorge",
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,29 +1,26 @@
 import { modelUser } from "../models/UserMo.js";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 
+export class UserService {
+  constructor() {}
+  async signupUser(data) {
+    let user = new modelUser(data);
+    user.password = await bcrypt.hash(user.password, 10);
 
-export class UserService{
-    constructor(){
-
-    }
-    async signupUser(data){        
-        let user = new modelUser(data)      
-        user.password = await bcrypt.hash(user.password, 10)          
-               
-        return await user.save() 
-    }
-    async deleteUser(id){        
-        await modelUser.findByIdAndDelete(id)
-    }
-    async updateUser(id, data){
-        await modelUser.findByIdAndUpdate(id, data)
-    }
-    async searchUser(id){
-        let user = await modelUser.findById(id)
-        return user
-    }
-    async searcAllUsers(){
-        let users = modelUser.find()
-        return users
-    }
+    return await user.save();
+  }
+  async deleteUser(id) {
+    await modelUser.findByIdAndDelete(id);
+  }
+  async updateUser(id, data) {
+    await modelUser.findByIdAndUpdate(id, data);
+  }
+  async searchUser(id) {
+    let user = await modelUser.findById(id);
+    return user;
+  }
+  async searcAllUsers() {
+    let users = modelUser.find();
+    return users;
+  }
 }
